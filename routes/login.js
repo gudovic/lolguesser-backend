518ba85c0ddcb89f58c25c2c79bf39db85ab48cb
maxmax <174373891+gudovic@users.noreply.github.com>
2025-07-11 20:10:45 +0200

    login issues mobile fix

--- routes/login.js
+++ routes/login.js
@@ -27,7 +27,7 @@ router.post('/', async (req, res) => {
         const token = jwt.sign(payload, process.env.JWT_SECRET, {
             expiresIn: '7d'
         });
-        const remember = req.body.remember;
+     //    const remember = req.body.remember;
         const maxAge = remember ? 30 * 24 * 60 * 60 * 1000 : 7 * 24 * 60 * 60 * 1000;
         res.cookie('token', token, {
             httpOnly: true,
@@ -36,7 +36,7 @@ router.post('/', async (req, res) => {
             maxAge: maxAge,
             path: '/'
         });
-
+    console.log(`✅ Cookie sent to user ${user.username}, remember: ${remember}`);
     res.status(200).json({ message: 'Login successful', username: user.username });
     } catch (err) {
     console.error(err);
