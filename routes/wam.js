20df3f9449078a2ed387b63e6fae06f1f4de81a3
maxmax <174373891+gudovic@users.noreply.github.com>
2025-08-10 15:51:17 +0200

    typo fix

--- routes/wam.js
+++ routes/wam.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const WamScore = require('../models/WamScore');
+const WamScore = require('../models/Wamscore');
 
 router.post('/', async (req, res) => {
   try {
